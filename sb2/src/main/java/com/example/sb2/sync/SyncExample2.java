268f27ac6bf9f65e79c63e2e165be89c93244966
Zhenning Lu <2436013662@qq.com>
2021-07-05 23:56:22 +0800

    save

--- sb2/src/main/java/com/example/sb2/sync/SyncExample2.java
+++ sb2/src/main/java/com/example/sb2/sync/SyncExample2.java
@@ -7,18 +7,18 @@ import java.util.concurrent.Executors;
 
 @Slf4j
 public class SyncExample2 {
-    // 修饰一个代码块
+    // 修饰一个类
     public void test1(int j) {
-        synchronized (this) {
-            for(int i = 0;i<10;++i) {
+        synchronized (SyncExample2.class) {
+            for(int i = 0;i<5;++i) {
                 log.info("test1 {} - {}",j, i);
             }
         }
     }
 
-    // 修饰一个方法
-    public synchronized void test2(int j) {
-        for(int i = 0;i<10;++i) {
+    // 修饰一个静态方法
+    public static synchronized void test2(int j) {
+        for(int i = 0;i<5;++i) {
             log.info("test2 {} - {}", j, i);
         }
     }
@@ -28,10 +28,10 @@ public class SyncExample2 {
         SyncExample2 syncExample2 = new SyncExample2();
         ExecutorService executorService = Executors.newCachedThreadPool();
         executorService.execute(() -> {
-            syncExample1.test2(1);
+            syncExample1.test1(1);
         });
         executorService.execute(() -> {
-            syncExample1.test2(2);
+            syncExample2.test1(2);
         });
     }
 }
